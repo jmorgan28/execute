75e0bf8a071803dfd990b32f7e9bc28669877614
Jackson Morgan <jmorgan@stuy.edu>
2016-11-18 11:29:28 -0500

    good start

---
+++ exec.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc,char * argy[]){
+  printf("Enter the program name:\n");
+  char a[50];
+  fgets(a,sizeof(a),stdin);
+  printf("%s\n", a); 
+  return 0;
+}
