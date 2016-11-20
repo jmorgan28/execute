3acd24bfc50765cd997d30b81abdb50e6dc2020a
Suwamik <spaul@stuy.edu>
2016-11-20 17:51:18 -0500

    fixed stuff

--- exec.c
+++ exec.c
@@ -12,38 +12,23 @@
 
 int main(int argc,char * argy[]){
   printf("What would you like to do?:\n");
-  char a[250];
-  fgets(a,sizeof(a),stdin);
+  char a[256];
   char *s = a;
-  int i = 0;
-  int k = 0;
-  while(s[i]){
-    i ++;
-    if(s[i] == ' '){
-      k ++;
-    }
-  }
+  fgets(a,sizeof(a),stdin);
+  a[strlen(a) - 1] = 0;
+
   
-  char * commands[k +2];
+  char * commands[256];
   int w = 0;
-  while(w < k + 1){
+  while(w < sizeof(s)){
     commands[w] = strsep(&s, " ");
     w ++;
   }
-  w = 0;
-  while(commands[k][w]){
-    w ++;
-  }
-  char bet[w];
-  w = 0;
-  while(commands[k][w] != '\n'){
-    bet[w] = commands[k][w];
-    w ++;
-  }
-  commands[k] = bet;
+  commands[w] = 0;
+  
   printf("%s\n", commands[0]);
   //printf("%s\n", commands[1]);
   execvp(commands[0],commands);
-  printf("work");
+  printf("works");
   return 0;
 }
